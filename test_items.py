cdb4acdf160007cf3064719732d134252808a268
Andrey Feklichev <feklichev@mail.ru>
2021-11-24 01:01:27 +0500

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_page_has_add_basket_button(browser):
+    browser.get(link)
+    WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket")))
+
