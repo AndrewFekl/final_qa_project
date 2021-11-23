49acf05f2eae4d7f83c917a732d13d58718818d8
Andrey Feklichev <feklichev@mail.ru>
2021-11-24 01:28:42 +0500

    Fixed test_items.py

--- test_items.py
+++ test_items.py
@@ -1,10 +1,13 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_has_add_basket_button(browser):
     browser.get(link)
-    WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket")))
+    time.sleep(30)
+    button = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket")))
+    assert button, "Add basket button isn't found"
 
