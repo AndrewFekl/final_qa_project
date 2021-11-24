e4d02fb22115ca7b30627777a17be82de4bc67f7
Andrey Feklichev <feklichev@mail.ru>
2021-11-24 16:36:18 +0500

    Modified test_main_page.py in PageObject style

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,11 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    page = MainPage(browser, link)
+    page.open()  # открываем страницу
+    page.go_to_login_page()
+    """browser.get(link)
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    login_link.click()"""
 
-def test_page_has_add_basket_button(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    button = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket")))
-    assert button, "Add basket button isn't found"
